e695905258a6e2c002b3eba36f8b1c7849e8144d
cluelessITstudent <112802134+cluelessITstudent@users.noreply.github.com>
2025-05-22 00:01:02 -0700

    Add files via upload

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://F83NSkLOEsgCX6ycJa_O_rw1amFPuWVf_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Life: A Journey of Learning"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "This course provides students with an engaging exploration of life&#39;s fundamental principles, challenges, and interconnected systems. Through interactive knowledge checks, quizzes, and activities, learners will develop a deeper understanding of biological processes, adaptation mechanisms, and the ethical considerations surrounding life. Designed for students at all levels, the course emphasizes active learning with an 80% passing requirement to ensure mastery of key concepts. Participants will gain insights into the cycle of life, the impact of human actions on ecosystems, and strategies for finding personal meaning and purpose. By the end of the course, students will be equipped with the knowledge and critical thinking skills to appreciate and navigate the complexities of life."
+};
